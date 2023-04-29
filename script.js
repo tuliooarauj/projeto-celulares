f786cb61bdf8e2b1c09f03076a9aed9e1bac6944
Tulio Oliveira <tulio_oliveira27@hotmail.com>
2023-04-29 14:58:17 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,96 @@
+var asusScript = window.document.getElementById('asusI')
+var realmeScript = window.document.getElementById('realmeI')
+var appleScript = window.document.getElementById('appleI')
+var samsungScript = window.document.getElementById('samsungI')
+var xioamiScript = window.document.getElementById('xiaomiI')
+var lgScript = window.document.getElementById('lgI')
+var huaweiScript = window.document.getElementById('huaweiI')
+var motorolaScript = window.document.getElementById('motorolaI')
+var sonyScript = window.document.getElementById('sonyI')
+
+function entrouAsus(){
+   
+    if(asusScript.style.display == 'none'){
+        asusScript.style.display = 'block'
+    }else{
+        asusScript.style.display = 'none'
+    }
+}
+function entrouApple(){
+   
+    if(appleScript.style.display == 'none'){
+        appleScript.style.display = 'block'
+    }else{
+        appleScript.style.display = 'none'
+    }
+}
+
+function entrouSamsung(){
+    
+    if(samsungScript.style.display == 'none'){
+        samsungScript.style.display = 'block'
+    }else{
+        samsungScript.style.display = 'none'
+    }
+}
+
+function entrouXiaomi(){
+    
+    if(xioamiScript.style.display == 'none'){
+        xioamiScript.style.display = 'block'
+    }else{
+        xioamiScript.style.display = 'none'
+    }
+}
+function entrouLg(){
+   
+    if(lgScript.style.display == 'none'){
+        lgScript.style.display = 'block'
+    }else{
+        lgScript.style.display = 'none'
+    }
+}
+function entrouHuawei(){
+    
+    if(huaweiScript.style.display == 'none'){
+        huaweiScript.style.display = 'block'
+    }else{
+        huaweiScript.style.display = 'none'
+    }
+}
+function entrouMotorola(){
+    
+    if(motorolaScript.style.display == 'none'){
+        motorolaScript.style.display = 'block'
+    }else{
+        motorolaScript.style.display = 'none'
+    }
+}
+function entrouSony(){
+    
+    if(sonyScript.style.display == 'none'){
+        sonyScript.style.display = 'block'
+    }else{
+        sonyScript.style.display = 'none'
+    }
+}
+function entrouRealme(){
+    
+    if(realmeScript.style.display == 'none'){
+        realmeScript.style.display = 'block'
+    }else{
+        realmeScript.style.display = 'none'
+    }
+}
+function clicouFora(){
+
+    asusScript.style.display = 'none'
+    appleScript.style.display = 'none'
+    samsungScript.style.display = 'none'
+    xioamiScript.style.display = 'none'
+    lgScript.style.display = 'none'
+    huaweiScript.style.display = 'none'
+    motorolaScript.style.display = 'none'
+    sonyScript.style.display = 'none'
+    realmeScript.style.display = 'none'
+}
